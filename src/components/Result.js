adf7e692adba577d2720721ab606c9c6d04238a1
Soham <sohamdas273@gmail.com>
2021-06-25 01:23:31 +0530

    deploy the quiz react app

---
+++ src/components/Result.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Result=({score,playAgain})=>(
+    <div className="score-board">
+        <div className="score">You Scored {score} out of 5 Correct Answers !!</div>
+        <button className="playBtn" onClick={playAgain}>Play Again !!</button>
+    </div>
+);
+
+export default Result;
